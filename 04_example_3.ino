e1a4c6e0b199ae7e5f4558d1690ee2f82308b433
t-t-k-i-m <hancity991@naver.com>
2025-09-10 09:41:18 +0900

    Create 04_example_3.ino

---
+++ 04_example_3.ino
@@ -0,0 +1,27 @@
+#define PIN_LED 13
+unsigned int count;
+int toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);    
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // 시리얼 연결 대기
+  }
+  Serial.println("Hello World!");
+  count = 0;
+  toggle = LOW;                 
+  digitalWrite(PIN_LED, toggle);
+}
+
+void loop() {
+  Serial.println(++count);      
+  toggle = toggle_state(toggle); 
+  digitalWrite(PIN_LED, toggle);
+  delay(1000);
+}
+
+int toggle_state(int t) {
+  if (t == LOW) return HIGH;  
+  else return LOW;            
+}
